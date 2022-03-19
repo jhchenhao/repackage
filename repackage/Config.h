71a2c7f77bee3ae97f00c49c70ed222aa5a79adc
jhch <jhtxchenhao@163.com>
2022-03-19 12:03:12 +0800

    fir commit

---
+++ repackage/Config.h
@@ -0,0 +1,17 @@
+//
+//  Config.h
+//  repackage
+//
+//  Created by 1234 on 2022/3/19.
+//
+
+#ifndef Config_h
+#define Config_h
+
+static NSString * const AAAAA                = @"AAAA";
+static NSString * const BBBBB                = @"BBBB";
+static NSString * const CCCCC                = @"CCCC";
+
+
+
+#endif /* Config_h */
